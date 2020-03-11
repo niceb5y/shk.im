a5e546e279ec802067830549a8582fecc2149aef
Seungho Kim <niceb5y@gmail.com>
2020-03-11 23:27:53 +0900

    Bump version

--- pages/_document.js
+++ pages/_document.js
@@ -3,35 +3,35 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <link rel="stylesheet" href="https://cdn.shk.im/webcore/v19.css" />
-          <script src="https://cdn.shk.im/webcore/v19.js" />
-        </Head>
-        <body>
-          <div className="container">
-            <Header />
-            <Main />
-            <Footer />
-          </div>
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta charSet="utf-8" />
+					<meta httpEquiv="x-ua-compatible" content="ie=edge" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1, shrink-to-fit=no"
+					/>
+					<link rel="stylesheet" href="https://cdn.shk.im/webcore/v20.css" />
+					<script src="https://cdn.shk.im/webcore/v20.js" />
+				</Head>
+				<body>
+					<div className="container">
+						<Header />
+						<Main />
+						<Footer />
+					</div>
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
